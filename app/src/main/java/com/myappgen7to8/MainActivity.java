e6462f3495d511257c1b40fb74921f32c391d51d
Aarti Patel <royal.arati10@gmail.com>
2021-12-27 19:50:57 +0530

    First Java Code

--- app/src/main/java/com/myappgen7to8/MainActivity.java
+++ app/src/main/java/com/myappgen7to8/MainActivity.java
@@ -3,12 +3,31 @@ package com.myappgen7to8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+EditText edtEmail;
+Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtEmail = findViewById(R.id.edt_email);
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String strEmail =  edtEmail.getText().toString();
+              // edtEmail.setError("Enter Email ID");
+                Toast.makeText(MainActivity.this,
+                        "Email id is  "+strEmail, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
